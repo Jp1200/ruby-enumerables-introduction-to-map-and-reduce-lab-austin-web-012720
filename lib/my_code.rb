7f934df7f62b45afcea6cf3f291d955e94bf1b86
Jack Parker <jprkr96@gmail.com>
2020-01-15 01:25:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -63,8 +63,6 @@ def reduce_to_any_true(source_array)
   while i < source_array.length do 
     if source_array[i] == Integer
       return true 
-    elsif source_array.all? != Integer
-      return false
     end
     i += 1 
   end
