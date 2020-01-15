8dad9a149b40cb4dd812aa0699188f648fdefa66
Jack Parker <jprkr96@gmail.com>
2020-01-15 00:36:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,37 +2,37 @@
 def map_to_negative(source_array)
   i = 0 
   new_array = []
-  while i < source_array.length {
+  while i < source_array.length do 
     new_array << source_array[i]*(-1)
     i += 1 
-  }
+  end
   return new_array
 end
 
 def map_to_no_change(source_array)
   i = 0 
   new_array = []
-  while i < source_array.length {
+  while i < source_array.length do 
     new_array << source_array[i]
     i += 1 
-  }
+  end
   return new_array
 end
 def map_to_double(source_array)
   i = 0 
   new_array = []
-  while i < source_array.length {
+  while i < source_array.length do
     new_array << source_array[i]*(2)
     i += 1 
-  }
+  end
   return new_array
 end
 def map_to_square(source_array)
   i = 0 
   new_array = []
-  while i < source_array.length {
+  while i < source_array.length do
     new_array << source_array[i]*source_array[i]
     i += 1 
-  }
+  end
   return new_array
 end
